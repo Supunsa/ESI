f56bdd065ab2514f7eb666a586f02459956e2f5c
Supunsa <supunsanimsu@hotmail.com>
2022-01-14 17:38:26 +0700

    test commit

--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,7 @@ const mysql = require('mysql');
 
 // GET home page.
 router.get('/', function (req, res) {
+  var data = []
   var con = mysql.createConnection({
     host: 'localhost',
     user: 'root',
@@ -14,10 +15,65 @@ router.get('/', function (req, res) {
 
   con.connect(function (err) { // เชื่อมต่อ Database
     if (err) throw err; // ถ้าเกิด Error ให้โยนค่าที่แจ้งออกมา
-    con.query("SELECT * FROM dbo_config JOIN dbo_raw ORDER BY dbo_raw.Id DESC limit 3 ", function (err, result) { // สั่งให้ไปเลือก table datalogs โดยดึงจาก id *** desc ไม่มั่นใจว่าเรียงจากล่าสุด ***
+    con.query("SELECT * FROM dbo_configs ", function (err, result) { // สั่งให้ไปเลือก table datalogs โดยดึงจาก id *** desc ไม่มั่นใจว่าเรียงจากล่าสุด ***
       if (err) throw err; // ถ้าไม่สามารถดึงข้อมูลมาจากที่เลือกได้ แจ้ง Error
 
-        return res.render('index', { result});
+      con.query("SELECT * FROM dbo_raws ORDER BY dbo_raws.Id DESC limit 1 ", function (err, value) { // สั่งให้ไปเลือก table datalogs โดยดึงจาก id *** desc ไม่มั่นใจว่าเรียงจากล่าสุด ***
+        if (err) throw err; // ถ้าไม่สามารถดึงข้อมูลมาจากที่เลือกได้ แจ้ง Error
+
+        if(value[0].data1 == null) data.push("Null")
+        else data.push(value[0].data1)
+
+        if(value[0].data2 == null) data.push("Null")
+        else data.push(value[0].data2)
+
+        if(value[0].data3 == null) data.push("Null")
+        else data.push(value[0].data3)
+
+        if(value[0].data4 == null) data.push("Null")
+        else data.push(value[0].data4)
+
+        if(value[0].data5 == null) data.push("Null")
+        else data.push(value[0].data5)
+
+        if(value[0].data6 == null) data.push("Null")
+        else data.push(value[0].data6)
+
+        if(value[0].data7 == null) data.push("Null")
+        else data.push(value[0].data7)
+
+        if(value[0].data8 == null) data.push("Null")
+        else data.push(value[0].data8)
+
+        if(value[0].data9 == null) data.push("Null")
+        else data.push(value[0].data9)
+
+        if(value[0].data10 == null) data.push("Null")
+        else data.push(value[0].data10)
+
+        if(value[0].data11 == null) data.push("Null")
+        else data.push(value[0].data11)
+
+        if(value[0].data12 == null) data.push("Null")
+        else data.push(value[0].data12)
+
+        if(value[0].data13 == null) data.push("Null")
+        else data.push(value[0].data13)
+
+        if(value[0].data14 == null) data.push("Null")
+        else data.push(value[0].data14)
+
+        if(value[0].data15 == null) data.push("Null")
+        else data.push(value[0].data15)
+
+        if(value[0].data16 == null) data.push("Null")
+        else data.push(value[0].data16)
+
+        // console.log(data);
+
+        return res.render('index', { result, data, value: value[0] });
+      });
+
     });
   });
 });
@@ -45,11 +101,11 @@ router.get('/get/:date/:endDate', function (req, res) {
     if (err) throw err; // ถ้าเกิด Error ให้โยนค่าที่แจ้งออกมา
     con.query(`SELECT * FROM datalogs WHERE Date BETWEEN '${day}' AND '${Endday}'`, function (err, result, fields) { // สั่งให้ไปเลือก table datalogs โดยดึงจาก id *** desc ไม่มั่นใจว่าเรียงจากล่าสุด ***
       if (err) throw err; // ถ้าไม่สามารถดึงข้อมูลมาจากที่เลือกได้ แจ้ง Error
-    con.end()
+      con.end()
       return res.render('getData', { datas: result, start: Startday, end: Endday }); // สั่งให้ไปประมวลผลไฟล์ index.ejs แล้วส่งค่าที่ได้จากการดึงไปด้วย
     });
   });
-  
+
 
 });
 
